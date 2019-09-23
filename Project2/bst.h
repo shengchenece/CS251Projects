65f17da1effcd1e18d0f5e6cf206b9a750eff489
shengchenece <35973678+shengchenece@users.noreply.github.com>
2019-09-22 20:16:23 -0500

    Part 1 of 5

---
+++ Project2/bst.h
@@ -0,0 +1,227 @@
+/*bsh.h*/
+
+//
+// Binary search tree
+//
+
+#pragma once
+
+#include <iostream>
+#include <algorithm>  // std::max
+
+using namespace std;
+
+template<typename TKey, typename TValue>
+class binarysearchtree
+{
+
+// BSTs now declared as:
+// binarysearchtree<int,MovieData> bstMoviesByID;
+struct MovieData
+{
+int PubYear;
+int Num5Stars;
+int Num4Stars;
+int Num3Stars;
+int Num2Stars;
+int Num1Stars;
+};
+
+private:
+  struct NODE
+  {
+    TKey    Key;
+    TValue  Value;
+    NODE*   Left;
+    NODE*   Right;
+  };
+
+  NODE* Root;  // pointer to root node of tree (nullptr if empty)
+  int   Size;  // # of nodes in the tree (0 if empty)
+
+  // 
+  // _inorder does the actual inorder traversal and output 
+  // to console.  Each key is output to the console followed
+  // by " ", including the last key.
+  //
+  void _inorder(NODE* cur)
+  {
+     if (cur == nullptr)
+       return;
+     else
+     {
+        _inorder(cur->Left);
+        cout << cur->Key << " " << cur->Value << endl;
+        _inorder(cur->Right);
+     }
+  }
+
+   //helper function for recursive height() function
+   int _height(NODE* rt)
+   {
+      if (rt == nullptr)
+      {
+         return -1;
+      }
+
+      int leftHeight = _height(rt->Left);
+      int rightHeight = _height(rt->Right);
+      return 1 + max(leftHeight, rightHeight);
+   }
+
+public:
+  //
+  // default constructor:
+  //
+  // Creates an empty tree.
+  //
+  binarysearchtree()
+  {
+    Root = nullptr;
+    Size = 0;
+  }
+
+  // 
+  // size:
+  //
+  // Returns the # of nodes in the tree, 0 if empty.
+  //
+  int size()
+  {
+    return Size;
+  }
+
+  //
+  // height
+  //
+  // Computes and returns height of tree; height of an empty tree is
+  // defined as -1.
+  //
+  int height()
+  {
+     return _height(Root);
+  }
+
+  // 
+  // search:
+  //
+  // Searches the tree for the given key, returning true if found
+  // and false if not.
+  //
+  TValue* search(TKey key)
+  {
+    NODE* cur = Root;
+
+    while (cur != nullptr)
+    {
+      if (key == cur->Key)  // already in tree
+        return &(cur->Value);
+
+      if (key < cur->Key)  // search left:
+      {
+        cur = cur->Left;
+      }
+      else
+      {
+        cur = cur->Right;
+      }
+    }//while  
+  
+    // if get here, not found
+    return nullptr;
+  }
+
+  //
+  // insert
+  //
+  // Inserts the given key into the tree; if the key has already been insert then
+  // the function returns without changing the tree.
+  //
+  void insert(TKey key, TValue value)
+  {
+     
+    NODE* prev = nullptr;
+    NODE* cur = Root;
+
+    //
+    // 1. Search to see if tree already contains key:
+    //
+    while (cur != nullptr)
+    {
+      if (key == cur->Key)  // already in tree
+        return;
+
+      if (key < cur->Key)  // search left:
+      {
+        prev = cur;
+        cur = cur->Left;
+      }
+      else                 // search right:
+      {
+        prev = cur;
+        cur = cur->Right;
+      }
+    }//while
+
+    //
+    // 2. if we get here, key is not in tree, so allocate
+    // a new node to insert:
+    // 
+
+    //
+    // allocate a new node, store key, initialize
+    // pointer fields:
+    NODE* newNode = new struct NODE();
+    newNode->Key = key;
+    newNode->Value = value;
+    newNode->Left = nullptr;
+    newNode->Right = nullptr;
+    
+    //
+    // 3. link in the new node:
+    //
+    // NOTE: cur is null, and prev denotes node where
+    // we fell out of the tree.  if prev is null, then
+    // the tree is empty and the Root pointer needs 
+    // to be updated.
+    //
+
+    //
+    // link in the new node, updating Root 
+    // pointer as appropriate
+    if (prev == nullptr)
+    {
+      Root = newNode;
+    }
+    else
+    {
+       if (key < prev->Key)
+       {
+         prev->Left = newNode;
+       }
+       else //if (key > prev->Key)
+       {
+          prev->Right = newNode;
+       }
+    }
+    // 4. update size and we're done:
+    Size++;
+  }
+
+  //
+  // inorder:
+  //
+  // Performs an inorder traversal of the tree, outputting
+  // the keys to the console.
+  //
+  void inorder()
+  {
+     cout << "Inorder: ";
+     
+     _inorder(Root);
+     
+     cout << endl;
+  }
+
+};
+
